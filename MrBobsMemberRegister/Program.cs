14d5e23292a13d2f8937b9951981901a7ead34b0
forNowhere <140905424+SquireofNowhere@users.noreply.github.com>
2025-02-11 23:04:29 +0200

    update
    update

--- MrBobsMemberRegister/Program.cs
+++ MrBobsMemberRegister/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+
 
 namespace MrBobsMemberRegister
 {
@@ -10,7 +13,30 @@ namespace MrBobsMemberRegister
     {
         static void Main(string[] args)
         {
+            // Connection string for SQL Server LocalDB
+            string connString = @"Server=(localdb)\MSSQLLocalDB;Integrated Security=true;";
 
+            DBConnect conn = new DBConnect(connString);
+            conn.Connect();
+
+            Console.WriteLine("Welcome to Mr Bob's Member Register");
+            Console.WriteLine("1. Database options");
+
+            //Switch for first menu choice
+            switch (int.Parse(Console.ReadLine()))
+            {
+                case 1:
+                    Console.Clear();
+                    Console.WriteLine("1. Check database tables");
+                    conn.CheckDBTables();
+                    break;
+
+                default:
+                    break;
+            }
+
+            Console.WriteLine("Press any to exit");
+            Console.ReadKey();
         }
     }
 }
